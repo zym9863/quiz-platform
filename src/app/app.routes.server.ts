c937b18fe0fa7360465852f2a3e86bc2a82809ff
zym <202213093023@cuc.edu.cn>
2025-04-19 14:56:03 +0800

    fix a bug

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,14 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { ServerRoute, RenderMode } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'quiz/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'results/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
